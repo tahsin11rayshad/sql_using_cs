7272a0cebc03d08b68f27c7287e31e14e846459d
sakhasib <adibsakhawat@iut-dhaka.edu>
2023-08-08 23:20:39 +0600

    init

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"type 'commands' to know the commands. or make any command");
+
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+                if(cmd == "exit")
+                {
+                    break;
+                }
+
+                else if(cmd == "commands") // ishmaam
+                {
+                    utility.print_commands();                
+                }
+
+                else if(cmd.Contains("create database"))
+                {
+                    string input = cmd;
+                    // Find the index where "create database" ends and get the substring after that
+                    int index = input.IndexOf("create database");
+                    if (index != -1)
+                    {
+                        string databaseName = input.Substring(index + "create database".Length).Trim();
+                        utility.create_database(databaseName);
+                        /*Console.WriteLine(databaseName);*/
+                    }
+                    else
+                    {
+                        Console.WriteLine("Command is not correct - 'create database your_database_name'");
+                    }
+                }
+            }
+                           
+
+
+
+            Console.WriteLine("Database Closed");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ utility.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database
+{
+    internal static class utility
+    {
+        public static string dir = @"C:\shuvro_database\";
+        public static string curr_dir;
+
+        
+
+        // To Create A new Database
+        public static void create_database(string dbname)
+        {
+            string targetDirectory = dir + @"/" + dbname + @"/";
+            try
+            {
+                Directory.CreateDirectory(targetDirectory);
+                curr_dir = targetDirectory;
+                Console.WriteLine("Database created successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        public static void print_commands()
+        {
+            string commands =   "exit - To Close the database\n"
+                              + "commands - To See Commands\n"
+                              + "";
+
+
+            Console.WriteLine(commands);
+        }
+
+
+    }
+}
